8c7ae039e8f51d440e5853ed8242604395a394d8
cybergenik <cybergenik@gmail.com>
2023-09-04 03:45:35 -0700

    cleaner

--- 2022/day4/part2.rs
+++ 2022/day4/part2.rs
@@ -5,21 +5,19 @@ fn main() -> Result<()> {
     let content = std::fs::read_to_string("input.txt")?;
     
     let mut total = 0;
-    for (_, line) in content.lines().map(str::trim).enumerate() {
-        line.split_once(",")
-            .map(|(l, r)| {
-                let l_set = l.split_once("-")
-                    .map(|(x, y)| (x.parse::<u8>().unwrap(), y.parse::<u8>().unwrap()))
-                    .map(|(s, e)| (s..e+1).collect::<HashSet::<u8>>())
-                    .expect("Fuck you");
-                let r_set = r.split_once("-")
-                    .map(|(x, y)| (x.parse::<u8>().unwrap(), y.parse::<u8>().unwrap()))
-                    .map(|(s, e)| (s..e+1).collect::<HashSet::<u8>>())
-                    .expect("Fuck you");
-                if !l_set.is_disjoint(&r_set) || !r_set.is_disjoint(&l_set) {
-                    total +=1
-                }
-            });
+    for line in content.lines().map(str::trim) {
+        let (l, r) = line.split_once(",").expect("Unable to split by comma");
+        let l_set = l.split_once("-")
+            .map(|(x, y)| (x.parse::<u8>().unwrap()..y.parse::<u8>().unwrap()+1))
+            .expect("Failed creating range set")
+            .collect::<HashSet::<u8>>();
+        let r_set = r.split_once("-")
+            .map(|(x, y)| (x.parse::<u8>().unwrap()..y.parse::<u8>().unwrap()+1))
+            .expect("Failed creating range set")
+            .collect::<HashSet::<u8>>();
+        if !l_set.is_disjoint(&r_set) || !r_set.is_disjoint(&l_set) {
+            total +=1
+        }
     }
     
     println!("{total}");
