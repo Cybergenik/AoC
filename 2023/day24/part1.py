33b0ae5f3c45e46ca71d5cebf8183577e0841d89
Luciano <cybergenik@gmail.com>
2024-07-24 22:14:45 -0400

    24

---
+++ 2023/day24/part1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def main():
+    with open("input.txt") as f:
+        content = f.readlines()
+
+    hailstones = []
+    for l in content:
+        
+if __name__ == "__main__":
+    raise SystemExit(main())
