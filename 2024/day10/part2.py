9fde2d399438861d6dee9aae7239cb5997a45033
Luciano <cybergenik@gmail.com>
2024-12-09 21:34:01 -0800

    day 10

---
+++ 2024/day10/part2.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python3
+
+from collections import defaultdict
+#from functools import cache
+#from collections import Counter
+
+def adj(graph, x, y):
+    for nx, ny in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+        if graph[(nx, ny)] != -1 and graph[(nx, ny)]-graph[(x, y)] == 1:
+            yield (nx, ny)
+
+def find_paths(graph, starts):
+    scores = defaultdict(lambda: 0)
+    def dfs(path):
+        if graph[path[-1]] == 9:
+            scores[path[0]] += 1
+            return
+        for v in adj(graph, *path[-1]):
+            dfs(path+[v])
+    for s in starts:
+        dfs([s])
+    return scores
+
+def main():
+    with open("input.txt") as f:
+        content = f.readlines()
+        
+    graph = defaultdict(lambda: -1)
+    starts = []
+    for y, l in enumerate(content):
+        for x, c in enumerate(l.strip()):
+            if c == ".":
+                continue
+            if c == "0":
+                starts.append((x,y))
+            graph[(x,y)] = int(c)
+    paths = find_paths(graph, starts)
+    total = 0
+    for k, v in paths.items():
+        total += v
+    print(total)
+if __name__ == "__main__":
+    raise SystemExit(main())
